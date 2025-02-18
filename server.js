57e552344c2045b40ca27e04e300df0c697bcb4b
Musab74 <Rmcbaxr@gmail.com>
2025-02-19 00:09:53 +0900

    Updated server.js and added new files

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
+const http = require("http");
 
 // 1 expressga kirib kelayotgan ma'lumotlarga bog'liq kodlar: KIRISH kodlari
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-const http = require("http");
+
 // 2: Session
 
 // 3 Beckend server side rendering frontend yasash traditionsal yasash: VIEW
-app.set("view", "views");
+app.set("views", "views");
 app.set("view engine", "ejs");   // biz ejs orqali frontend yasaymiz
  
 //4 Routing codes
-app.get("/", function(req, res){
-    res.end(`<h1>hello world</h1>`);
+
+app.post("/create-item", (req, res) => {
+    console.log(req);
+    res.json({test: "success"})
 });
 
-app.get("/", function(req, res){
-    res.end(`<h1>siz sovg'alar bo'limidasiz</h1>`);
+app.get("/", function (req, res) {
+    res.render("xarid");
 });
+
 
 const server = http.createServer(app);
 let PORT = 3000;
