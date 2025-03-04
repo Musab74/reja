f555b930af4ada1e5e265a359e9098c0137c7714
Musab74 <Rmcbaxr@gmail.com>
2025-03-04 23:09:18 +0900

    feat: all is done

--- app.js
+++ app.js
@@ -4,57 +4,85 @@ const mongodb = require("mongodb");
 //app
 const app = express();
 
-const db = require ("./server").db();
+const db = require("./server").db();
 
 
 // 1 expressga kirib kelayotgan ma'lumotlarga bog'liq kodlar: KIRISH kodlari
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // 2: Session
 
 // 3 Beckend server side rendering frontend yasash traditionsal yasash: VIEW
 app.set("views", "./views");
 app.set("view engine", "ejs");   // biz ejs orqali frontend yasaymiz
- 
+
 //4 Routing codes
 
 app.post("/create-item", (req, res) => {
     console.log("user entered/create item");
     const new_reja = req.body.reja;
-    db.collection("plans").insertOne({reja: new_reja}, (err,data) => {
+    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
         console.log(data.ops);
-       res.json(data.ops[0]);
+        res.json(data.ops[0]);
     });
 });
-
+//delete
 app.post("/delete-item", (req, res) => {
-    const id=req.body.id;
-   db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)},
-   function (err, data) {
-    res.json({state: "success"});
-   }
-);
+    const id = req.body.id;
+    db.collection("plans").deleteOne({ _id: new mongodb.ObjectId(id) },
+        function (err, data) {
+            res.json({ state: "success" });
+        }
+    );
+})
+
+//edit-button
+// app.post("/edit-item", (res, req) => {
+//     const data = req.body;
+//     console.log(data);
+//     res.end("done");
+
+// })
+
+app.post("/edit-item",  (req, res) => {
+    const data = req.body;
+    console.log(data);
+    db.collection("plans").findOneAndUpdate(
+        {_id: new mongodb.ObjectId(data.id)}, 
+        {$set: {reja: data.new_input}}, 
+        function(err, data) {
+         res.json({state: "success"});
+    }  
+    );
+});
+
+app.post("/delete-all", (req, res) => {
+    if(req.body.delete_all) {
+        db.collection("plans").deleteMany(function() {
+        res.json({state: "hamma rejalar o'chirildi"});
+        });
+    }
 })
 
 
 app.get("/", function (req, res) {
     console.log("user entered");
     db.collection("plans")
-    .find()
-    .toArray((err, data) => {
-        if (err) {
-            console.log(err);
-            res.end("something went wrong");
+        .find()
+        .toArray((err, data) => {
+            if (err) {
+                console.log(err);
+                res.end("something went wrong");
 
-        }else {
-        
-            res.render("reja", {items: data});
-            
-        }
-    });
-   
+            } else {
+
+                res.render("reja", { items: data });
+
+            }
+        });
+
 });
 
 module.exports = app; 
--- public/browser.js
+++ public/browser.js
@@ -2,7 +2,7 @@
 console.log("Frontend ishga tushdi")
 
 function itemTemplate(item) {
-   return `<li 
+    return `<li 
    class="list-group-item d-flex align-items-center justify-content-between">
     <span class="item-text"> ${item.reja} 
     </span>    
@@ -19,40 +19,66 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
     e.preventDefault();
 
     axios
-    .post("/create-item" , {reja: createField.value})
-    .then((response) => {
-        document
-        .getElementById("item_ul")
-        .insertAdjacentHTML("beforeend", itemTemplate(response.data)); 
-        createField.value = "";
-        createField.focus();
+        .post("/create-item", { reja: createField.value })
+        .then((response) => {
+            document
+                .getElementById("item_ul")
+                .insertAdjacentHTML("beforeend", itemTemplate(response.data));
+            createField.value = "";
+            createField.focus();
 
 
-    })
-    .catch((err) => {
-        console.log("please try again", err);
-    } );
+        })
+        .catch((err) => {
+            console.log("please try again", err);
+        });
 
 })
 
-document.addEventListener("click", function(e) {
+document.addEventListener("click", function (e) {
     //delete section
     console.log(e.target);
-    
-    if(e.target.classList.contains("delete")) {
-        if (confirm ("You want delete?")){
-         axios
-         .post("/delete-item", {id: e.target.getAttribute("data-id")})
-         .then((response) => {
-            console.log(response.data);
-            e.target.parentElement.parentElement.remove();
-            
-         })
-         .catch((err)=> {
-            console.log("please try again", err);
-         });
 
-         }
+    if (e.target.classList.contains("delete")) {
+        if (confirm("You want delete?")) {
+            axios
+                .post("/delete-item", { id: e.target.getAttribute("data-id") })
+                .then((response) => {
+                    console.log(response.data);
+                    e.target.parentElement.parentElement.remove();
+
+                })
+                .catch((err) => {
+                    console.log("please try again", err);
+                });
+
         }
+    }
     // edit section
-    });
+
+    if (e.target.classList.contains("edit")){
+        let userInput =   prompt("O'zgartirish kiriting", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
+        if (userInput){
+           axios.post("/edit-item", {id: e.target.getAttribute("data-id"),
+       new_input: userInput,
+         })
+         .then( response => {
+           console.log(response.data);
+           e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput;
+         } )
+         .catch( err => {
+           console.log("qayta harakat qiling");
+         } )
+        }
+       }
+   });
+   
+   document.querySelector(".clean-all").addEventListener("click", function() {
+   axios.post("/delete-all", {delete_all: true }).then(response => {
+       alert(response.data.state);
+       document.location.reload();
+   })
+
+});
+
+
