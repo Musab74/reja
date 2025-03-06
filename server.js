3b556151f6c562ac7218c9a6a6b2cb282f102892
Musab74 <Rmcbaxr@gmail.com>
2025-03-06 21:09:50 +0900

    feat: ready to deploy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongodb.connect(connectionString,
         
         const app = require("./app");
         const server = http.createServer(app);
-        const PORT = 3000;        server.listen(PORT, () => {
+        const PORT = 3001;        server.listen(PORT, () => {
             console.log(`The server is successfully running on port: ${PORT}, http://localhost:${PORT}`);
         });
     }
